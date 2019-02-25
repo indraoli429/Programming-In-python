ac0ff7211e6450f062b7cb840982e091c298a225
Indra Bahadur Oli <42300489+khillsman3@users.noreply.github.com>
2019-02-26 00:07:30 +0545

    New files create

---
+++ AlgebaricExpression1.py
@@ -0,0 +1,4 @@
+x=int(input("Enter number"))
+y=int(input("Enter number"))
+z=(x**3+3*x**2*y+3*x*y**2+y**3)
+print("The answer is",z)
---
+++ Greater.py
@@ -0,0 +1,8 @@
+a=int(input("Num1:"))
+b=int(input("Num2:"))
+if a == b:
+    print("Equal")
+elif a>b:
+    print ("Number a is greater and its value is",a)
+else:
+    print("Number b is greater and its value is",b)
