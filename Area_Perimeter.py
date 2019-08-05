9012683e5a5718c194eb388c19ff14e662fdd794
Indra Bahadur Oli <indraoli429@gmail.com>
2019-08-05 19:15:32 +0545

    deleted
    duplicate files

--- Area_Perimeter.py
+++
@@ -1,6 +0,0 @@
-L = int(input("Enter the value of Lenght : " ))
-B = int(input("Enter the value of Breadth : " ))
-Area = L * B
-Perimeter = 2*(L+B)
-print ("The Area is ", Area)
-print ("The perimeter is ", Perimeter)
--- ascending.py
+++
@@ -1,5 +0,0 @@
-a = int(input("Num1:"))
-b = int(input("Num2:"))
-if a>b:
-    a,b = b,a #Swap the value
-print (a,b)
